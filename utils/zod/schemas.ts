dcea8fe2e70d44377ab2755bf4e551b01b35db7c
Guy Kabilio <guyka00@gmail.com>
2024-01-14 18:06:24 +0200

    Zod Schema fixes

--- utils/zod/schemas.ts
+++ utils/zod/schemas.ts
@@ -1,7 +1,7 @@
 import {z} from 'zod' 
 
 export const signUpSchema = z.object({
-  fullname: z.string().min(2, "At least 2 characters").max(10, "At most 10 characters"),
+  fullname: z.string().min(2, "At least 2 characters").max(20, "At most 20 characters"),
   email: z.string().email() ,
   password: z.string().min(10,'Password needs to be at list 10 characters'),
   confirmPassword: z.string()
@@ -13,3 +13,10 @@ export const signUpSchema = z.object({
 export const formSchema = z.object({
   content: z.string()
 })
+
+export const imageFormSchema = z.object({
+  prompt:z.string().min(1,{message:'Image prompt is required'}),
+  amount:z.string().min(1),
+  resolution:z.string().min(1)
+})
+
