e8db134abd617beb6d4d82eb446623c52df11298
Guy Kabilio <guyka00@gmail.com>
2024-01-14 18:10:05 +0200

    Adding types

--- utils/types.ts
+++ utils/types.ts
@@ -1,17 +1,45 @@
 import { ToastPosition } from 'react-toastify'
-import {formSchema, signUpSchema} from './zod/schemas' 
+import {formSchema, imageFormSchema, signUpSchema} from './zod/schemas' 
 import {z} from 'zod'
 import { AxiosError } from 'axios'
 
 export type SignUpSchema = z.infer< typeof signUpSchema> 
 export type FormSchemaType = z.infer< typeof formSchema> 
+export type ImageSchemaType = z.infer< typeof imageFormSchema>
 export type ErrorType = typeof AxiosError | Error
 
+
+export type User = Partial<{
+  fullname:string
+  email:string
+  password:string
+  provider:[string]
+  freeUses:number
+  premium:boolean
+  image:string
+}>
+
 export type MessageModel = {
-  id?:string | number
   content:string
   role:'user' | 'system'
 }
+
+export type ImageType = {
+  url: string;
+};
+
+export type MusicType = {
+  audio?: string;
+  spectrogram?:string
+  content?: string;
+}
+
+export type VideoType = {
+  src?:string
+  content?:string
+}
+
+
 
 
 export type ServiceCard = {
