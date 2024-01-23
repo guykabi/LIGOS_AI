0bb0ef0005e95da97d165cb81a226b3db163ddc5
Guy Kabilio <guyka00@gmail.com>
2024-01-23 21:10:31 +0200

    auth logic

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -5,7 +5,8 @@ import CredentialsProvider from "next-auth/providers/credentials"
 import connectDB from "../../libs/mongodb";
 import User from "../../libs/models/User"
 import bcrypt from 'bcrypt'
-import axios from "axios"
+import Axios from '../../api-instance'
+
 
 export const authOptions = {
 
@@ -30,16 +31,19 @@ export const authOptions = {
           if(!credentials?.email || !credentials?.password ) return null
           
           await connectDB()
-
+          
+        
           const user = await User.findOne({ email: credentials.email })
           
           if(!user) return {error:'No such user exist'}
 
-          if(!user?.password) return {error:'Please connect with SSO provider'}
+          if(!user?.password) return {error:'You are only able to connect with SSO'}
           
           const match = await bcrypt.compare(
             credentials.password, user.password
           )
+
+          if(!match) return {error:'Incorrect password'}
 
           if (match) {
             delete user.password
@@ -71,54 +75,87 @@ export const authOptions = {
     })
   ],
   
-  // pages: {
-  //   signIn: "/signIn"
-  // },
+  pages: {
+    signIn: "/signIn"
+  },
 
   callbacks: {
 
     async signIn({user,account}){
-
+       
        if(account?.provider === 'google' || account?.provider === 'github'){
-
+       
         try{
+
+          await connectDB()
+         
+          const isUser = await User.findOne({ email: user.email })
+
+          if(isUser) {
+            user.id = isUser._id.toHexString()
+            user.premium = isUser.premium
+            return user
+          }
 
             let body = {
               fullname:user.name,
               email:user.email,
               freeUses:0,
               premium:false,
-              provider:account.provider
+              provider:[account.provider]
             }
 
-            const {data} = await axios.post('http://localhost:3000/api/users',body)
+            const {data} = await Axios('/api/users',body)
             user.id = data?.user?._id
             return user
 
         }catch(error){
-          return null
+          throw new Error(user.error)
         }
        }
 
-       if(user.error) return null
+       if(user.error) throw new Error(user.error)
 
        return user
     },
-    async jwt({ token, user }) {
-      
+    async jwt({ token, user,trigger,session }) {
+
       if (user) {
+        
         token.id = user.id
         token.name = user?.fullname ? user.fullname : user.name
         token.image = user?.picture ? user.picture : user.avatar_url
+        token.premium = user?.premium
+      
         return token
       }
+
+      //Update jwt token
+      if (trigger === "update" && Object.keys(session).length) {
+        let property = Object.keys(session)[0]
+        token[property] = session[property]
+        
+        return token
+      }
+
       return token
     },
-    async session({ session, token }) {
-      
+
+    async session({ session, token,trigger,newSession }) {
+
       if (token) {
         session.user.id = token.id
         session.user.image = token.image ? token.image : token.avatar_url
+        session.user.premium = token?.premium
+       
+        return session
+      }
+
+      //Update session - DB operations
+      if (trigger === "update" && Object.keys(newSession).length) {
+        let property = Object.keys(newSession)[0]
+        session[property] = newSession[property]
+        
         return session
       }
       return session
