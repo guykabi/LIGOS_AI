31a4a72b2de5924beff0abe45201529e7f66befe
Guy Kabilio <guyka00@gmail.com>
2024-02-05 20:19:55 +0200

    fixing code div

--- components/CodeForm/Markdown/markdown.tsx
+++ components/CodeForm/Markdown/markdown.tsx
@@ -1,25 +1,39 @@
 // 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import  styles from './markdown.module.scss'
 import ReactMarkdown from 'react-markdown'
+import { IoCopy,IoCopyOutline } from "react-icons/io5";
 
 
-const Markdown = ({content}:{content:any}) => {
+const Markdown = ({content}:{content:string}) => {
+  const [isCopied,setIsCopied]=useState<boolean>()
+
+  const copyToClipboard = (e:any) => {
+    navigator.clipboard.writeText(e)
+    if(!isCopied)setIsCopied(true)
+  }
+  
   return (
    <ReactMarkdown
    className={styles.markdown}
    components={{
-    pre:({node,...props})=>(
-      <span className={styles.pre}>
-        <pre  {...props}/>
-      </span>
-    ),
-    code: ({node,...props})=>(
-      <span className={styles.code}>
-        <code  {...props}/>
-      </span>
-    )
+    code(props) {
+      const {children, className, node, ...rest} = props
+      const match = /language-(\w+)/.exec(className || '')
+      return match ? (
+        <code {...rest} className={styles.code}>
+          <p className={styles.copyIcon} onClick={()=>copyToClipboard(children)}>
+           {isCopied ? <IoCopy size={15} color="black"/> : <IoCopyOutline size={15} color="black"/>}
+          </p>
+          {children}
+        </code>
+      ) : (
+        <code {...rest} style={{fontWeight:'600'}}>
+          `{children}`
+        </code>
+      )
+    }
    }}
    >
     {content}
