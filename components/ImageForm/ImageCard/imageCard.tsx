e6857b8c91ec80547b33cf845ba4469d72e8d163
Guy Kabilio <guyka00@gmail.com>
2024-02-05 20:21:10 +0200

    download logic

--- components/ImageForm/ImageCard/imageCard.tsx
+++ components/ImageForm/ImageCard/imageCard.tsx
@@ -1,34 +1,38 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import Card from "@mui/material/Card";
-import CardMedia from "@mui/material/CardMedia";
 import CardActions from "@mui/material/CardActions";
-import IconButton, { IconButtonProps } from "@mui/material/IconButton";
+import IconButton from "@mui/material/IconButton";
 import { MdDownload } from "react-icons/md";
-import { GiShare } from "react-icons/gi";
-import Skeleton from "@mui/material/Skeleton";
 import Image from "next/image";
-import Share from "@/components/Share/share";
+import { saveAs } from 'file-saver'
+
 
 type ImageCardProps = {
   src: string;
+  searchKey:string | undefined
 };
 
-const ImageCard = ({ src }: ImageCardProps) => {
-  const [openShare, setOpenShare] = useState<boolean>(false);
+const ImageCard = ({ src,searchKey }: ImageCardProps) => {
+ 
+
+  const downloadImage = () => {
+    let savedName = `${searchKey}.png`
+    saveAs(src,savedName)
+  }
+
+
   return (
     <Card sx={{ width: "100%", height: 260, position: "relative" }}>
-      {openShare ? <Share shareUrl={src} title="Using LIGOS AI" /> : null}
       <Image alt="image" src={src} fill style={{ objectFit: "cover" }} />
       <CardActions disableSpacing>
-        <IconButton aria-label="download" onClick={() => window.open(src)}>
-          <MdDownload />
-        </IconButton>
-        <IconButton
-          aria-label="share"
-          onClick={() => setOpenShare((prev) => !prev)}
-        >
-          <GiShare />
+        <IconButton 
+        sx={{
+          background:'white'
+        }}
+        aria-label="download" 
+        onClick={downloadImage}>
+          <MdDownload color="black" />
         </IconButton>
       </CardActions>
     </Card>
