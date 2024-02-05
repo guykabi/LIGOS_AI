63919191f3caa453dfead8159294c22e8c9c7fba
Guy Kabilio <guyka00@gmail.com>
2024-02-05 20:13:01 +0200

    settings schema

--- utils/zod/schemas.ts
+++ utils/zod/schemas.ts
@@ -1,28 +1,55 @@
-import {z} from 'zod' 
+import { z } from "zod";
 
+const MAX_FILE_SIZE = 5000000;
+export const ACCEPTED_IMAGE_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+];
 
 export const signInSchema = z.object({
   email: z.string().email(),
-  password: z.string()
-})
+  password: z.string(),
+});
 
-export const signUpSchema = z.object({
-  fullname: z.string().min(2, "At least 2 characters").max(20, "At most 20 characters"),
-  email: z.string().email() ,
-  password: z.string().min(10,'Password needs to be at list 10 characters'),
-  confirmPassword: z.string()
-}).refine(data => data.password === data.confirmPassword ,{
-  message:"Passwords must match",
-  path:["confirmPassword"]
-}) 
+export const signUpSchema = z
+  .object({
+    name: z
+      .string()
+      .min(2, "At least 2 characters")
+      .max(20, "At most 20 characters"),
+    email: z.string().email(),
+    password: z.string().min(10, "Password needs to be at list 10 characters"),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords must match",
+    path: ["confirmPassword"],
+  });
 
 export const formSchema = z.object({
-  content: z.string()
-})
+  content: z.string(),
+});
 
 export const imageFormSchema = z.object({
-  prompt:z.string().min(1,{message:'Image prompt is required'}),
-  amount:z.string().min(1),
-  resolution:z.string().min(1)
-})
+  prompt: z.string().min(1, { message: "Image prompt is required" }),
+  amount: z.string().min(1),
+  resolution: z.string().min(1),
+});
 
+export const detailsFormSchema = z.object({
+  name: z
+    .string()
+    .min(2, "At least 2 characters")
+    .max(20, "At most 20 characters"),
+  email: z.string().email(),
+  image: z
+    .any()
+    .refine((files) =>  files?.[0]?.size <= MAX_FILE_SIZE || !files.length, `Max file size is 5MB.`
+    )
+    .refine(
+      (files) => files.length && ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type) || !files.length,
+          ".jpg, .jpeg, .png and .webp files are accepted."
+    ).optional()
+});
