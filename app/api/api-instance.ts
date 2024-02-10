895a5909b243105f53c4c5f6e8803cecffbeb5f9
Guy Kabilio <guyka00@gmail.com>
2024-02-10 20:55:49 +0200

    env

--- app/api/api-instance.ts
+++ app/api/api-instance.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 const Axios = axios.create({
     withCredentials: true,
     baseURL: process.env.NODE_ENV === 'production' ? 
-      process.env.NEXT_PUBLIC_URL_PROD:
-      process.env.NEXT_PUBLIC_URL
+      process.env.NEXT_PUBLIC_BASE_URL_PROD:
+      process.env.NEXT_PUBLIC_BASE_URL
 });
 
 export default Axios;
