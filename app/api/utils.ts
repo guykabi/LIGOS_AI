1dc8df3cf2d7ca6da68c19b8b471f2185dce892f
Guy Kabilio <guyka00@gmail.com>
2024-02-17 16:34:45 +0200

    hashing + email utils

--- app/api/utils.ts
+++ app/api/utils.ts
@@ -3,6 +3,9 @@ import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "./auth/[...nextauth]/options";
 import Message, { MessageType, Service } from "./libs/models/Message";
+import {send} from '@emailjs/nodejs' 
+import { createHash } from "crypto";
+
 
 export const handleServerSession = async (
   req: NextRequest,
@@ -70,3 +73,23 @@ export const handleInsertMessage = async (
     throw new Error("Internal Error");
   }
 };
+
+
+export const handleHashing = (token:string) =>{
+  return createHash("sha256")
+    .update(token)
+    .digest("hex");
+}
+
+
+export const emailHandler = async(msg:any) => {
+  try{
+    const {status,text} = await send(process.env.EMAILJS_SERVICE_ID!, process.env.EMAILJS_TEMPLATE_ID!, msg, {
+      publicKey: process.env.EMAILJS_PUBLIC_KEY!,
+      privateKey:process.env.EMAILJS_PRIVATE_KEY
+    })
+     return status
+  }catch(err){
+    return 'Failed sending email. Try again'
+  }
+}
